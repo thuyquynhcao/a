49cf60cdd4a805c392c1f27e9686d8b0a8c36db3
thuyquynhcao <caoquynh2@gmail.com>
2020-03-29 15:19:51 +0000

    todoApp

--- src/App.js
+++ src/App.js
@@ -3,11 +3,28 @@ import ToDoListItem from "./ToDoListItem.js"
 import './App.css';
 
 class App extends Component {
+    //hay su dung state de quan ly todoList(mang)
   render() {
     return (
       <div className="App">
+      <form className = "App-form"
+      //3.onSubmit={
+          // 取得する　id la element cua title va description
+          //su dung this.setState(), them data vao todoList
+          //element は　クリアしてください
+     // }
+      >
+      // them form nhap title and description
+      //them 登録ボタン vao cung 
         <div>
+        //4.todoList の要素数分のtodoListItem を呼び出す(su dung map)
+        
           <ToDoListItem
+          //2.
+          title = "テスト"
+          description = "実験です"
+          //5.
+          // onClick setState を使って　todoList から要素を削除する
           />
         </div>
       </div>
--- src/ToDoListItem.js
+++ src/ToDoListItem.js
@@ -3,8 +3,15 @@ import './ToDoListItem.css';
 
 class ToDoListItem extends Component {
   render() {
+      const {
+          //1.5
+          title,
+          description,
+          ...props
+      } = this.props;
+      //1.thêm title and description vào HTML 
     return (
-      <div className="ToDoListItem">
+      <div className="ToDoListItem"{...props}>
         <div className="ToDoListItem-title"></div>
         <div className="ToDoListItem-description"></div>
       </div>
